a250b76c00ed5150acceecc7392519f52e9e79f6
Duncan Macleod <macleoddm@cardiff.ac.uk>
2024-09-03 17:27:20 +0100

    update windows build paths
    addresses failure where the pure python sources were being installed into the wrong place

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,9 +5,15 @@
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
 
 :: get mixed path (forward slash) form of prefix so host prefix replacement works
-set "LIBRARY_PREFIX_M=%LIBRARY_PREFIX:\=/%"
+set "PREFIX_M=%PREFIX:\=/%"
 
-meson setup builddir --wrap-mode=nofallback --buildtype=release --prefix=%LIBRARY_PREFIX_M% --backend=ninja -Dpython=%PREFIX%\python.exe -Dpygi-overrides-dir=%SP_DIR%\gi\overrides
+meson setup builddir ^
+  --backend=ninja ^
+  --buildtype=release ^
+  --libdir=%LIBRARY_LIB% ^
+  --prefix=%PREFIX_M% ^
+  --wrap-mode=nofallback ^
+  -Dpython=%PREFIX%\python.exe
 if errorlevel 1 exit 1
 
 ninja -v -C builddir -j %CPU_COUNT%
